e02bb6d8fd183532014fca7595151b268ce87746
gyesswhat <h2weirdo@icloud.com>
2024-10-04 18:33:56 +0200

    chore: 파일 초기 세팅

---
+++ src/main/java/com/example/hearhere/HearhereApplication.java
@@ -0,0 +1,13 @@
+package com.example.hearhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hearhere/HearhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hearhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
