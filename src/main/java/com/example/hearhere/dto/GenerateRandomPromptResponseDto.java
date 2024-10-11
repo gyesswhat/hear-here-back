9caed748f98321621425d3ae9baeb3fd147947aa
gyesswhat <h2weirdo@icloud.com>
2024-10-11 12:11:15 +0200

    feat: ASMR 생성 기능 - 랜덤 프롬프트 생성 기능 추가

---
+++ src/main/java/com/example/hearhere/dto/GenerateRandomPromptResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.hearhere.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class GenerateRandomPromptResponseDto {
+    private ArrayList<String> prompts;
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO example_prompt(prompt) VALUES
+                                       ('Dense forest, birds chirping, waterfall cascading down'),
+                                       ('Summer Hawaii wave, dolphin calls, people giggling'),
+                                       ('City drive at midnight, radio sound, car driving'),
+                                       ('First Night in a Strange Metropolis, Quiet room, opening window sound, sudden burst of loud city noise, feeling of loneliness in a separate space.'),
+                                       ('Lively Ice Cream Shop, Ice clattering, scooping ice cream, upbeat jazz music, light laughter from people around.'),
+                                       ('Empty University Classroom on a Rainy Day, Light footsteps, sound of writing notes, soft chair, warm hot chocolate, steady rain at 11°C, productive study session.'),
+                                       ('Snowy Winter Day, (Cheerful / calm) carols, rolling snow for a snowman, excited mood, occasional jingle bells sound.'),
+                                       ('Late Night Café in Paris, Clinking coffee cups, light French chatter, pen on paper, rain tapping on windows, warm ambient light.'),
+                                       ('Early Morning Hike in the Mountains, Gravel crunching underfoot, crisp air, distant hawk call, breeze rustling leaves, river flowing softly.'),
+                                       ('Quiet Library in the Afternoon, Pages turning softly, faint air conditioner hum, muffled footsteps, occasional whispers, calming smell of old books.'),
+                                       ('Breezy Beachside Picnic at Sunset, Waves lapping the shore, wind flapping the picnic blanket, light laughter, distant seagulls, fresh sea air.'),
+                                       ('Stormy Night at a Cozy Cabin, Rain hitting the roof, distant thunder rumbling, crackling fireplace, creaking rocking chair, tea being sipped.');
