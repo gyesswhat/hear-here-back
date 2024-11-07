67b3b5ac4d048da47dc2e82e6e2ec814a162a620
gyesswhat <h2weirdo@icloud.com>
2024-11-07 14:49:45 +0100

    refactor: 필요 없는 .sql 파일 제거

--- src/main/resources/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS example_prompt (
-                                              prompt_id BIGINT AUTO_INCREMENT PRIMARY KEY,  -- 기본 키, 자동 증가
-                                              prompt VARCHAR(255) NOT NULL                  -- 프롬프트 내용을 저장할 필드, NULL을 허용하지 않음
-);
